7ca81f58ed32427c682bb8cf191d9b2a66bcf105
chansung park <deep.diver.csp@gmail.com>
2024-04-16 13:17:43 +0900

    0.0.5 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="hllama",
-    version="0.0.4",
+    version="0.0.5",
     description="hllama provides some useful utility functions for LLM.",
     long_description=long_description,
     long_description_content_type="text/markdown",
